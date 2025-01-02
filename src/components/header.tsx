4cfdef39306984b3524cfbc717d12b97db793112
Beaskyy <afeez.akintobi@yahoo.com>
2025-01-02 12:47:24 +0100

    authentication in Nextjs 15

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,21 @@
-import { SignInButton, UserButton } from '@clerk/nextjs'
-import React from 'react'
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import React from "react";
 
 const Header = () => {
   return (
-    <div className='flex justify-between items-center bg-white py-2 text-gray-800 w-full px-8'>
+    <div className="flex justify-between items-center bg-white py-2 text-gray-800 w-full px-8">
       <p>logo</p>
       <div className="flex items-center gap-5">
-      <SignInButton mode='modal' />
-      <UserButton />
+        <SignedOut>
+          <SignInButton mode="modal" />
+        </SignedOut>
+
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
